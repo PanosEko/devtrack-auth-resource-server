4968a903a41cc21414faf679336ef127b2fca265
PanosEko <93736094+PanosEko@users.noreply.github.com>
2023-10-26 19:47:47 +0300

    allow origins

--- src/test/java/com/panoseko/devtrack/DevtrackApplicationTests.java
+++ src/test/java/com/panoseko/devtrack/DevtrackApplicationTests.java
@@ -3,7 +3,7 @@ package com.panoseko.devtrack;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DevtrackApplicationTests {
 
 	@Test
