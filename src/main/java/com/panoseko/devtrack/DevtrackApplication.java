c8f23ab89dc3d8161c11063c7b584ad7c4715afa
PanosEko <93736094+PanosEko@users.noreply.github.com>
2023-10-30 15:35:22 +0200

    allow origins

--- src/main/java/com/panoseko/devtrack/DevtrackApplication.java
+++ src/main/java/com/panoseko/devtrack/DevtrackApplication.java
@@ -22,10 +22,7 @@ public class DevtrackApplication {
 			public void addCorsMappings(@NonNull CorsRegistry registry) {
 				registry.addMapping("/**")
 						.allowedMethods("*")
-//						.allowedOrigins("http://localhost:8080","http://localhost:3000")
-						.allowedOrigins("https://app.getpostman.com", "https://devtrack-frontend.vercel.app",
-								"api.devtrack.dedyn.io", "https://devtrack.dedyn.io", "https://api.devtrack.dedyn.io",
-								"http://api.devtrack.dedyn.io","http://devtrack.dedyn.io")
+						.allowedOrigins("https://devtrack.dedyn.io")
 						.allowedHeaders("*")
 						.allowCredentials(true)
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
